2061098155efa42217ee934218478bdc01cd1658
vipulshete <vipulshete724@gmail.com>
2022-07-16 23:47:51 +0530

    welcome to snake and ladder game

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelab.sankeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Play Snake and Ladder Game");
+
+	}
+
+}
