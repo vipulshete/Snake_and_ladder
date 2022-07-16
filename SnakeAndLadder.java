4502ff8f41a14e176ba8e10b93a84a7f3e2982e8
vipulshete <vipulshete724@gmail.com>
2022-07-17 00:01:08 +0530

    modified : start position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,7 +4,20 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Play Snake and Ladder Game");
-
+		String playerOne = "vipul";
+		System.out.println("Name of player is " + playerOne);
+		
+		int start = 0;
+		int block = 100;
+		
+		for (int i=start; i<block; i++) {
+			
+			if (i==0) {
+				System.out.println("Its starting point");
+			}
+		}
+	
 	}
-
+	
+	
 }
