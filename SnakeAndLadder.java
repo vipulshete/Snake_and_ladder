538003b86ba82c5f2b646dc29414dbb6d5bee450
vipulshete <vipulshete724@gmail.com>
2022-07-17 00:11:12 +0530

    modified : rolling die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,26 @@
 package com.bridgelab.sankeandladder;
 
+import com.bridgelab.snakeAndLadder.RollDieOne;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Play Snake and Ladder Game");
 		String playerOne = "vipul";
 		System.out.println("Name of player is " + playerOne);
+		
+		
+		
+		SnakeAndLadder snakeLadder = new SnakeAndLadder();
+		snakeLadder.dieOne();
+		
+	
+	}
+	
+	void dieOne() {
+		
+		int playerOneDice = (int) Math.floor(Math.random() * 10) % 7; 
+		System.out.println("Die number is " + playerOneDice);
 		
 		int start = 0;
 		int block = 100;
@@ -16,8 +31,6 @@ public class SnakeAndLadder {
 				System.out.println("Its starting point");
 			}
 		}
-	
 	}
-	
 	
 }
